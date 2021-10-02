97fe5118e17c805fb4f3c7152b4b1d732b8ca79b
Anikesh-pixelkare <anikesh.pixelkare@gmail.com>
2021-10-02 21:46:51 +0530

    sdfghjikl

--- server.ts
+++ server.ts
@@ -10,7 +10,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
+app.get('/', (req: any, res: any) => {
   res.send('hello world!');
 })
 
