ac41c4efc6c75d22b2a99b2ea0c32452d4c3a1a5
Anikesh-pixelkare <anikesh.pixelkare@gmail.com>
2021-10-02 20:54:49 +0530

    server changes

--- server.ts
+++ server.ts
@@ -1,10 +1,17 @@
 import {ServerConfig} from "./classes/server.config";
 
-const http = require('http');
-const serverConfig = new ServerConfig();
-const app = serverConfig.app;
+// const http = require('http');
+// const serverConfig = new ServerConfig();
+// const app = serverConfig.app;
 
-const port = process.env.PORT || 3000;
-export const myServer = app.listen(port, () => {
-    serverConfig.init();
-});
+// const port = process.env.PORT || 3000;
+// export const myServer = app.listen(port, () => {
+//     serverConfig.init();
+// });
+const http = require('http');
+
+http.createServer(function (req: any, res: any) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  console.log('node done');
+  res.end('Hello World!');
+}).listen(8080);
